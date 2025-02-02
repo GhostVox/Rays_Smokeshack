38abb0071106538de5beccd572027ded9eb3e654
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-02-01 20:27:48 -0500

    first edition main page with css is done

---
+++ index.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    let menu = document.getElementById("dropdownMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+document.addEventListener("click", function(event) {
+    let menu = document.getElementById("dropdownMenu");
+    let menuIcon = document.getElementById("menu");
+
+    if (event.target !== menu && event.target !== menuIcon) {
+        menu.style.display = "none";
+    }
+});
