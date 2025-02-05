8d7ed8f8e87eeb3887f5733c476ae9c6a357cc9b
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-02-04 23:51:57 -0500

    fixed the misplace of the gallery.js in the index.js i reverted back to my code now the drop menu is fixed

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-import { loopPhotos, nextPhoto, previousPhoto } from './gallery.js';
-
-document.addEventListener('DOMContentLoaded', function () {
-  loopPhotos();
-});
-
-const galleryContainer = document.getElementById('gallery');
-const forwardButton = document.getElementById('fowardButton');
-const backButton = document.getElementById('backButton');
-
-forwardButton.addEventListener('click', nextPhoto);
-backButton.addEventListener('click', previousPhoto);
 function toggleMenu() {
-  let menu = document.getElementById('dropdownMenu');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  let menu = document.getElementById("dropdownMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
-document.addEventListener('click', function (event) {
-  let menu = document.getElementById('dropdownMenu');
-  let menuIcon = document.getElementById('menu');
+document.addEventListener("click", function(event) {
+  let menu = document.getElementById("dropdownMenu");
+  let menuIcon = document.getElementById("menu");
 
   if (event.target !== menu && event.target !== menuIcon) {
-    menu.style.display = 'none';
+      menu.style.display = "none";
   }
 });
